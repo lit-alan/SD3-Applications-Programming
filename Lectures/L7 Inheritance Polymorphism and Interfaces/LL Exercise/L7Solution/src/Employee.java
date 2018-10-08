d043299ed6886c9d9fbacad1e1971b25239f878e
lit-alan <Alan.Ryan@lit.ie>
2018-10-08 15:25:12 +0100

    Added Emp class

---
+++ Lectures/L7 Inheritance Polymorphism and Interfaces/LL Exercise/L7Solution/src/Employee.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author alan.ryan
+ */
+public class Employee {
+    
+    private double payRate;
+    private String SSN;
+
+    public Employee(double payRate, String SSN) {
+        this.payRate = payRate;
+        this.SSN = SSN;
+    }
+    
+    
+}
