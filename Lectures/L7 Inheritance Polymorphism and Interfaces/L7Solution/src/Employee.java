1fb0200521b3e46fa1dfe9b5f998fd7db5c78a50
lit-alan <Alan.Ryan@lit.ie>
2018-10-08 16:40:33 +0100

    Updated Emp class

--- Lectures/L7 Inheritance Polymorphism and Interfaces/L7Solution/src/Employee.java
+++ Lectures/L7 Inheritance Polymorphism and Interfaces/L7Solution/src/Employee.java
@@ -1,5 +1,15 @@
 
 public class Employee {
+   private int age;
+   private double salary;
+
+    public Employee() {
+    }
+
+    public Employee(int age, double salary) {
+        this.age = age;
+        this.salary = salary;
+    }
    
     
 }
