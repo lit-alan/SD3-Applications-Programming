c837bc27b53b5b40a0a09b6019d17227884cb829
lit-alan <Alan.Ryan@lit.ie>
2022-09-22 11:33:36 +0100

    Added Solutions For Lecture Four Exercises

---
+++ Source/L4 Methods/Lecture4_Exercises_Source/src/main/java/com/lit/src/Exercise1.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lit.src;
+
+import java.text.DecimalFormat;
+import java.text.Numberat;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Alan.Ryan
+ */
+public class Exercise1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        NumberFormat nf = NumberFormat.getCurrencyInstance();
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("\t\tWELCOME TO LIT CARPARKS\n");
+        System.out.println("Enter duration of stay (enter -1 to quit)");
+
+        double duration = sc.nextDouble();
+
+        while (duration != -1) {
+            System.out.println("Charge is " + nf.format(calculateCharges(duration)));
+
+            System.out.println("\nEnter duration of stay (enter -1 to quit)");
+
+            duration = sc.nextDouble();
+        }
+        
+        System.out.println("GOODBYE");
+
+    }
+
+    static double calculateCharges(double duration) {
+        double totalCharge = 2.0;
+
+        if (duration > 3) {
+            totalCharge += (Math.ceil(duration) - 3) * 0.5;
+        }
+
+        if (totalCharge > 10.00) {
+            totalCharge = 10.00;
+        }
+
+        return totalCharge;
+    }
+
+}
