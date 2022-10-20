ebfa6bca58a42974532efd917b26aac102032859
lit-alan <Alan.Ryan@lit.ie>
2022-10-20 17:23:33 +0100

    Added Solution To Lab Ex4

---
+++ Source/Lectures/L8 Generics and Lambdas/Lab Exercises/Lab Ex 4/Lab_Ex_5/src/main/java/classes/LabEx1.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sd3.com.src;
+
+import java.util.Random;
+
+/**
+ *
+ * @author alan.ryan
+ */
+public class LabEx1 {
+
+    public static void main(String[] args) {
+
+        //task one 
+        MathOperation mod = (int a, int b) -> a % b;
+        System.out.println("10 mod 5 = " + operate(10, 5, mod));
+		
+        //task two
+        MathOperation rand = (int min, int max)
+                -> {
+            Random r = new Random();
+            return r.nextInt((max - min) + 1) + min;
+        };
+
+        System.out.println("Rand Value between 0 and 20 is " + operate(0, 20, rand));
+
+        
+        //task three
+	MathOperation add2 = (int a, int b) -> a + b;
+        System.out.println("3 + 4 + 1 = " + operate(operate(3, 4, add2), 1, add2));
+
+        //task four
+		 MathOperation mult2 = (int a, int b) -> a * b;
+        System.out.println("2 * 2 * 2 = " + operate(operate(2, 2, mult2), 2, mult2));
+
+        //task five
+        MathOperation larger = (int a, int b)
+                -> {
+            if (a > b) {
+                return a;
+            } else {
+                return b;
+            }
+        };
+
+        System.out.println("Larger of 3 and 4 is " + operate(4, 3, larger));
+
+        //task six
+        MathOperation smaller = (int a, int b)
+                -> {
+            if (a < b) {
+                return a;
+            } else {
+                return b;
+            }
+        };
+
+        System.out.println("Smaller of 3 and 4 is " + operate(4, 3, smaller));
+
+        //task seven
+        System.out.println("Larger of 3, 4 and 5 is " + operate(operate(4, 3, larger), 5, larger));
+
+        //task eight
+        System.out.println("Smaller of 3, 4 and 5 is " + operate(operate(4, 3, smaller), 5, smaller));
+    }
+
+    static int operate(int a, int b, MathOperation operation) {
+        return operation.test(a, b);
+    }
+
+}
---
+++ Source/Lectures/L8 Generics and Lambdas/Lab Exercises/Lab Ex 4/Lab_Ex_5/src/main/java/interfaces/MathOperation.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sd3.com.src;
+
+/**
+ *
+ * @author alan.ryan
+ */
+public interface MathOperation {
+    int test(int a, int b);
+}
