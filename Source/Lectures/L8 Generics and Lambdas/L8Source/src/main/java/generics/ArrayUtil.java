8c52184c3d2163e316198f279f6b1fe6179e666a
lit-alan <Alan.Ryan@lit.ie>
2023-10-23 11:11:51 +0100

    Added Source For Lecture 8 - Generics and Lambdas

---
+++ Source/Lectures/L8 Generics and Lambdas/L8Source/src/main/java/generics/ArrayUtil.java
@@ -0,0 +1,16 @@
+package generics;
+
+public class ArrayUtil {
+public static <T> void swap(T[] array, int i, int j) {
+        T temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    public static <T> T[] swap(int i, int j, T... values) {
+        T temp = values[i];
+        values[i] = values[j];
+        values[j] = temp;
+        return values;
+    }
+}
