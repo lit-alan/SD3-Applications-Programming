27372f382a469f204201074af8a6077611229d6e
lit-alan <Alan.Ryan@lit.ie>
2023-10-25 08:54:31 +0100

    Added Solutions for Lab Ex 5 and 6

---
+++ Source/Lab Exercises/Lab Ex 6 (Lambdas Wtih Generics)/LabEx_Five_Solution/src/main/java/v1/CountOperationForIntArr.java
@@ -0,0 +1,5 @@
+package v1;
+
+public interface CountOperationForIntArr {
+  boolean test(int value);
+}
---
+++ Source/Lab Exercises/Lab Ex 6 (Lambdas Wtih Generics)/LabEx_Five_Solution/src/main/java/v1/CountOperationForStringArr.java
@@ -0,0 +1,5 @@
+package v1;
+
+public interface CountOperationForStringArr {
+  boolean test(String value);
+}
